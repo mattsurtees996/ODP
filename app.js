0bd98a8c0ad0e7cf9c5055752ef2a9fc56174f53
mattsurtees996 <matthew.surtees@icloud.com>
2023-02-09 13:36:53 +0000

    finsihed project

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let totalClicks = 0;
+let maxClicks = 5;
+
 function Product(name, src) {
   this.name = name;
   this.src = src;
@@ -53,16 +56,74 @@ function renderImages() {
   img1.src = Product.allProducts[index1].src;
   img2.src = Product.allProducts[index2].src;
   img3.src = Product.allProducts[index3].src;
-}
 
-renderImages();
+  img1.alt = Product.allProducts[index1].name;
+  img2.alt = Product.allProducts[index2].name;
+  img3.alt = Product.allProducts[index3].name;
+
+  Product.allProducts[index1].views++;
+  Product.allProducts[index2].views++;
+  Product.allProducts[index3].views++;
+}
 
 function handleClick(event) {
   if (event.target === imgContainer) {
     alert("Please click one of the images, not inbetween the images");
+    return;
   }
-  for (let i = 0; i < Product.allProducts.length; i++) {}
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    if (event.target.alt === Product.allProducts[i].name) {
+      Product.allProducts[i].clicks++;
+      break;
+    }
+  }
+
+  totalClicks++;
+  if (totalClicks === maxClicks) {
+    alert("Thank you for voting!");
+    imgContainer.removeEventListener("click", handleClick);
+    renderChart();
+    return; // end the function
+  }
+
+  renderImages();
 }
 
 const imgContainer = document.getElementById("img-container");
 imgContainer.addEventListener("click", handleClick);
+
+function renderChart() {
+  const theChart = document.getElementById("chart");
+  let labels = [];
+  let viewsData = [];
+  let clicksData = [];
+
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    labels.push(Product.allProducts[i].name);
+    viewsData.push(Product.allProducts[i].views);
+    clicksData.push(Product.allProducts[i].clicks);
+  }
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "# of Views",
+        data: viewsData,
+        borderWidth: 1,
+      },
+      {
+        label: "# of Votes",
+        data: clicksData,
+        borderWidth: 1,
+      },
+    ],
+  };
+  const config = {
+    type: "pie",
+    data: data,
+  };
+  new Chart(theChart, config);
+}
+
+renderImages();
